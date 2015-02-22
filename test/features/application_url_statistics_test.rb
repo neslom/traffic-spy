51d1e5170f87f2eb7d0e663f94509250688fe601
Markus Olsen <molsen13@gmail.com>
2015-02-22 14:47:02 -0700

    Client can view popular user agents. Closes #29 and closes #35

--- test/features/application_url_statistics_test.rb
+++ test/features/application_url_statistics_test.rb
@@ -91,7 +91,9 @@ module TrafficSpy
 
     def test_most_popular_user_agents
       visit '/sources/google/urls/blog'
-
+      within("#most_popular_user_agents") do
+        assert page.has_content?("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17")
+      end
     end
 
   end
