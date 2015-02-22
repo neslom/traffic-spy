4e15e45f4b3beb280d847e52fbc82c8b00b97f67
Markus Olsen <molsen13@gmail.com>
2015-02-21 22:44:39 -0700

    Show average url response time. Closes #32

--- test/features/application_url_statistics_test.rb
+++ test/features/application_url_statistics_test.rb
@@ -17,6 +17,7 @@ module TrafficSpy
       post '/sources', 'identifier=google&rootUrl=http://google.com'
       post '/sources/google/data', 'payload={"url":"http://google.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":37,"referredBy":"http://google.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
       post '/sources/google/data', 'payload={"url":"http://google.com/blog","requestedAt":"2013-02-16 21:38:28 -0700","respondedIn":87,"referredBy":"http://google.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
+      post '/sources/google/data', 'payload={"url":"http://google.com/blog","requestedAt":"2013-02-15 21:38:28 -0700","respondedIn":37,"referredBy":"http://google.com","requestType":"GET","parameters":[],"eventName": "socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
     end
 
     def test_returns_error_if_url_does_not_exist
@@ -31,16 +32,26 @@ module TrafficSpy
       end
     end
 
-    def test_can_return_maximum_response_time
+    def test_can_return_longest_response_time
       visit '/sources/google/urls/blog'
       within("ul") do
         assert page.has_content?(87)
+        assert page.has_content?(/longest|Longest/)
       end
     end
 
-    def test_can_return_minimum_response_time
+    def test_can_return_shortest_response_time
       visit '/sources/google/urls/blog'
       within("ul") do
+        assert page.has_content?(/shortest|Shortest/)
+        assert page.has_content?(37)
+      end
+    end
+
+    def test_shows_average_response_time
+      visit '/sources/google/urls/blog'
+      within("ul") do
+        assert page.has_content?(/average|Average/)
         assert page.has_content?(37)
       end
     end
