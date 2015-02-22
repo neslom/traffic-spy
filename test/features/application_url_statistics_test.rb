eab5fa4aff40a60161aa805dbdd080c9d30e87a3
Markus Olsen <molsen13@gmail.com>
2015-02-22 15:03:59 -0700

    Delete comment from test

--- test/features/application_url_statistics_test.rb
+++ test/features/application_url_statistics_test.rb
@@ -98,24 +98,3 @@ module TrafficSpy
 
   end
 end
-# A client is able to view URL specific data at the following address:
-#
-# http://yourapplication:port/sources/IDENTIFIER/urls/RELATIVE/PATH
-#
-# http://yourapplication:port/sources/google/urls/blog
-# http://yourapplication:port/sources/google/urls/article/1
-# http://yourapplication:port/sources/google/urls/about
-#
-#
-# When the url for the identifier does exists:
-#
-#     Longest response time
-#     Shortest response time
-#     Average response time
-#     Which HTTP verbs have been used
-#     Most popular referrrers (most popular reffered_by)
-#     Most popular user agents
-#
-# When the url for the identifier does not exist:
-#
-#     Message that the url has not been requested
